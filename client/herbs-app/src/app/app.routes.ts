ac0d22f1bcbeb0d043cddc59bdc9a4a2d98c9c59
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-13 22:37:38 +0200

    fixed navigation

--- client/herbs-app/src/app/app.routes.ts
+++ client/herbs-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'catalog', component: CatalogComponent },
 ];
--- client/herbs-app/src/app/header/header.component.ts
+++ client/herbs-app/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CatalogComponent } from '../catalog/catalog.component';
+import { HomeComponent } from '../home/home.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
