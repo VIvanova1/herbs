e69773b40257f6cdfa90ced0ad642bcf811c033b
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-14 10:42:58 +0200

    create component

--- client/herbs-app/src/app/app.routes.ts
+++ client/herbs-app/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { DetailsComponent } from './details/details.component';
+import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'details', component: DetailsComponent },
+    { path: 'new', component: CreateComponent },
 ];
