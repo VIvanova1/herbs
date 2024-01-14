231a31689cb6411b5d8f865f7ddea1437ec4f5a9
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-14 11:05:35 +0200

    login register sample

--- client/herbs-app/src/app/app.routes.ts
+++ client/herbs-app/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { DetailsComponent } from './details/details.component';
 import { CreateComponent } from './create/create.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
     { path: 'details', component: DetailsComponent },
     { path: 'new', component: CreateComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
 ];
