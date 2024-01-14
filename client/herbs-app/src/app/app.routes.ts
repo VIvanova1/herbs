3ed72eb45d07adc44c2c22526bd10e918fc6333f
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-14 10:19:53 +0200

    details page sample

--- client/herbs-app/src/app/app.routes.ts
+++ client/herbs-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'catalog', component: CatalogComponent },
+    { path: 'details', component: DetailsComponent },
 ];
