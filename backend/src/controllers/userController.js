91c9a55d0956af631e47ee4e45227df1ea32077e
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-21 16:55:07 +0200

    login/ register

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,8 +4,7 @@ const User = require("../models/User");
 const userManager = require("../managers/userManager");
 const errorHandler = require("../utils/error");
 const jwt = require("jsonwebtoken");
-const config = require("../config");
-
+const {SECRET, TOKEN_KEY} = require("../config");
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -14,43 +13,52 @@ router.post("/register", async (req, res) => {
   const user = new User({ name, email, password: hashedPass });
 
   try {
-
-    const userExist = await userManager.findOne({email});
+    const userExist = await userManager.findOne({ email });
     if (userExist) {
       throw errorHandler(409, "User already exist!");
     }
 
-   const newUser= await userManager.register(user);
-   const token = await generateToken(newUser);
+    const newUser = await userManager.register(user);
 
-   res.cookie(config.TOKEN_KEY, token,{httpOnly:true});
-   res.status(200).json(token);
-
+    const token = await generateToken(newUser);
+    res.cookie(TOKEN_KEY, token, { httpOnly: true, secure: true });
+    res.status(200).json(token);
 
   } catch (err) {
     res.status(err.statusCode).json(err.message);
   }
 });
 
-// router.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
 
-//   try {
-//     const token = await userManager.login(email, password);
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body
 
-//     res.cookie(TOKEN_KEY, token);
-//   } catch (err) {}
-// });
+  try {
+    // const token = req.cookies[TOKEN_KEY];
+    const user = await userManager.findOne({ email });
+    if(!user){
+      throw errorHandler(404, 'User not found')
+    }
 
+    const validPass = bcrypt.compareSync(password, user.password);
+    if(!validPass){
+      throw errorHandler(401, 'Email or password is incorrect.')
+    }
 
-
+    const token =  await generateToken(user);
+    res.cookie(TOKEN_KEY, token, { httpOnly: true, secure: true });
+    res.status(200).json(token);
+  } catch (err) {
+    res.status(err.statusCode).json(err.message);
+  }
+});
 
 async function generateToken(user) {
   const payload = {
     _id: user._id,
     email: user.email,
   };
-  const token = await jwt.sign(payload, config.SECRET, { expiresIn: "1d" });
+  const token = await jwt.sign(payload, SECRET, { expiresIn: "1d" });
   return token;
 }
 
--- backend/src/managers/userManager.js
+++ backend/src/managers/userManager.js
@@ -2,6 +2,5 @@ const User = require("../models/User");
 
 
 exports.findOne = (userData) => User.findOne({ email: userData.email });
-// exports.findOne = (email) => console.log(email);;
 
 exports.register = (user) => User.create(user);
